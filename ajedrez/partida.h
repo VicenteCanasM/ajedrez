e82753b974992d498d1bf857b48ff3a05d4a2bf8
VicenteCanasM <48983287+VicenteCanasM@users.noreply.github.com>
2019-04-09 12:47:17 +0200

    Añadidos ficheros en carpeta

---
+++ ajedrez/partida.h
@@ -0,0 +1,24 @@
+#ifndef PARTIDA_H
+#define PARTIDA_H
+#include <vector>
+#include <iostream>
+#include "piezas.h"
+#include "tablero.h"
+#include "funciones.h"
+using namespace std;
+
+class partida {
+public:
+    vector <peon> v_peon;
+    vector <caballo> v_caballo;
+    vector <alfil> v_alfil;
+    vector <torre> v_torre;
+    vector <rey> v_rey;
+    int turno;
+public:
+    partida();
+    ~partida();
+    vector<vector<bool>> comprobar_mov_escaque(tablero, int, vector<peon>, vector<caballo>, vector<alfil>, vector<torre>, vector<rey>);
+};
+
+#endif // PARTIDA_H
