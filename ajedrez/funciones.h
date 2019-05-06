71284fe641a0df10999388b18ea4dd7ce2d2b0c0
JesusDiazMontarroso <48983298+JesusDiazMontarroso@users.noreply.github.com>
2019-05-06 18:48:46 +0200

    Add files via upload

--- ajedrez/funciones.h
+++ ajedrez/funciones.h
@@ -4,8 +4,6 @@
 
 bool comprobar_limites(int, int);
 
-bool comprobar_amenaza(tablero, int, int, int, vector<peon>, vector<caballo>, vector<alfil>, vector<torre>, vector<rey>, vector<dama>);
-
-void movimiento(tablero, QIcon, QIcon, QPushButton, QPushButton, bool, pair<int,int>, pair<int,int>);
+bool comprobar_amenaza(tablero, int, int, int, vector<peon>, vector<caballo>, vector<alfil>, vector<torre>, vector<dama>);
 
 QString nuevo_icono(int);
